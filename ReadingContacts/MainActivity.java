91897d27f51569d172d07b9982d8d099faeb05f6
Shreyas K <53894147+Shreyas999@users.noreply.github.com>
2020-04-01 15:30:21 +0530

    Update MainActivity.java

--- ReadingContacts/MainActivity.java
+++ ReadingContacts/MainActivity.java
@@ -1,61 +1,75 @@
-package com.example.s1contactlistview;
-
+package com.example.contentmad;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
 import android.content.ContentResolver;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.os.Build;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView et=(TextView) findViewById(R.id.textView);
-   /* private String[] mColumnProjection = new String[]{ContactsContract.Contacts.DISPLAY_NAME_PRIMARY,
-            ContactsContract.Contacts.HAS_PHONE_NUMBER};
+    TextView display_txt;
+    ImageView iv;
+    LinearLayout linearLayout,linearLayout1;
+    private String[] mColumnProjection = new String[]{
+            ContactsContract.Contacts.DISPLAY_NAME_PRIMARY
+            ,ContactsContract.Contacts.HAS_PHONE_NUMBER};
     private String mSelectionCluse = ContactsContract.Contacts.DISPLAY_NAME_PRIMARY + " = ?";
-    private String[] mSelectionArguments = new String[]{"Aditya"};
-    private String mOrderBy = ContactsContract.Contacts.DISPLAY_NAME_PRIMARY +"DESC";
-   */
+    private String[] mSelectionArguments = new String[]{"Papa"};
+    private String mOrderBy = ContactsContract.Contacts.DISPLAY_NAME_PRIMARY + "DESC";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b=(Button)findViewById(R.id.button);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_CONTACTS}, 0);
-                } else {
-                    StringBuffer buffer = new StringBuffer();
-                    ContentResolver contentResolver = getContentResolver();
-                    Cursor cursor;
-                    cursor = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null,
-                            null, null);
-                    if (cursor != null && cursor.getCount() > 0) {
-                        while (cursor.moveToNext()) {
-                            String name = cursor.getString(cursor.getColumnIndex(
-                                    ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                            String no = cursor.getString(cursor.getColumnIndex(
-                                    ContactsContract.CommonDataKinds.Phone.NUMBER));
-                            buffer.append(name);
-                            buffer.append(no);
-                        }
-                        et.setText(buffer);
-                    } else {
-                        et.setText("No Contacts in device");
-                    }
-                }
 
+
+    }
+    public void loadContactNames(View v) {
+        linearLayout=(LinearLayout)findViewById(R.id.ol);
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cur = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null,
+                null, null, null);
+        assert cur != null;
+        if (cur.getCount() > 0) {
+            while (cur.moveToNext()) {
+                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                Log.i("Names", name);
+
+                if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0)
+                {
+                    Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = "+ id,null, null);
+                    assert phones != null;
+                    while (phones.moveToNext()) {
+                        String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        String photo = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_URI));
+                        Log.i("Number", phoneNumber+photo);
+                        if (photo!=null)
+                        {
+                            Uri imgUri=Uri.parse(photo);
+                            final ImageView imageView= new ImageView(this);
+                            final TextView textView=new TextView(this);
+                            textView.setText(name+" "+phoneNumber);
+                            imageView.setImageURI(null);
+                            imageView.setImageURI(imgUri);
+                            linearLayout.addView(textView);
+                            linearLayout.addView(imageView);
+                        }
+                    }
+                    phones.close();
+                }
             }
-        });
+            cur.close();
+        }
+        else
+        {
+            display_txt.setText("No Contacts in device");
+        }
     }
-    }
+}
